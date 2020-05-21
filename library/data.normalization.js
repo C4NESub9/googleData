3ee8b2645af488a5ab71fc46d966effea6472092
alex.javarotti.developer@gmail.com <alex.javarotti.developer@gmail.com>
2020-05-21 12:32:58 -0600

    RequestData9 fixed

--- library/data.normalization.js
+++ library/data.normalization.js
@@ -20,7 +20,7 @@ async function requestData4(serie1, serie2, serie3, serie4) {
 
 async function requestData9(serie1, serie2, serie3, serie4, serie5, serie6, serie7, serie8, serie9) {
     try {
-        let [data_serie1, data_serie2, data_serie3, data_serie4] = await Promise.all([
+        let [data_serie1, data_serie2, data_serie3, data_serie4, data_serie5, data_serie6, data_serie7, data_serie8, data_serie9] = await Promise.all([
             fetch(serie1).then(response => response.text().then(text => text)),
             fetch(serie2).then(response => response.text().then(text => text)),
             fetch(serie3).then(response => response.text().then(text => text)),
